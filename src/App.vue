213385e8cd1d6460e859aa80969c3370a1d5f032
liva <liva1992@163.com>
2020-08-01 14:27:37 +0800

    完善 aside 切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,13 @@
 </template>
 
 <script lang='ts'>
-import { ref, provide } from 'vue';
+import { ref, provide } from "vue";
 export default {
   name: "App",
-  setup(){
-    const asideVisible=ref(false)
-    provide('asideVisible',asideVisible)
-  }
+  setup() {
+    const width = document.documentElement.clientWidth;
+    const asideVisible = ref(width <= 500 ? false : true);
+    provide("asideVisible", asideVisible);
+  },
 };
 </script>
--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleAside">LOGO</div>
+    <div class="logo" >LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside" @click="toggleAside"></span>
   </div>
 </template>
 <script lang='ts'>
@@ -41,9 +41,26 @@ export default {
       margin: 0 1em;
     }
   }
+  > .toggleAside {
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
   @media (max-width:500px){
-    .menu{display: none;}
-    .logo{margin:0 auto}
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
+    }
   }
 }
 </style>
