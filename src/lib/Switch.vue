0e2105c38d542a00d2f90d99b8d423c191ed8afc
liva <liva1992@163.com>
2020-08-02 20:56:07 +0800

    Swith 添加切换动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -34,6 +34,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+   transition: left 250ms;
 }
 button.checked {
   background: blue;
@@ -41,4 +42,7 @@ button.checked {
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
 }
+button:focus{
+    outline: none;
+  }
 </style>
