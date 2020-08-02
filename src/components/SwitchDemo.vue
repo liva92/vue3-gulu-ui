76a914a42977e5cd9244fba060f15f43b9e7ac20
liva <liva1992@163.com>
2020-08-02 20:30:17 +0800

    Switch 组件添加 HTML

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>Switch 的文档</div>
+  <div><Switch /></div>
 </template>
+<script lang="ts">
+
+import Switch from '../lib/Switch.vue'
+export default {
+  components: {Switch}  
+}
+</script>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,7 @@
+<template>
+  <button> <span></span> swith</button>
+</template>
+
+<style lang="scss" scoped>
+ 
+</style>
