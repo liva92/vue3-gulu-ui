f8c69d52d79a6bb25d6a11f2cd09ff9254e3fe7f
liva <liva1992@163.com>
2020-08-14 15:03:54 +0800

    创建 button 组件

---
+++ src/lib/Button.vue
@@ -0,0 +1,18 @@
+<template>
+<div :size='size'>
+<button v-bind="rest">
+  <slot></slot>
+  </button>
+</div>
+  
+</template>
+
+<script>
+export default {
+    inheritAttrs:false,
+    setup(props,context){
+const {size,...rest}=context.attrs
+return {size,rest}
+    }
+}
+</script>
