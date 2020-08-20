66b0d807f1b0528a3564d7214802186a4e511f0c
liva <liva1992@163.com>
2020-08-20 15:51:45 +0800

    支持 level 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>Button 示例</div>
-  <h1>示例一 theme</h1>
-  <div>
+  <h1>Button 示例</h1>
+
+  <h2>示例1 </h2>
+  <div class="box">
     <Button>默认按钮</Button>
     <Button theme="button">button 按钮</Button>
-    <Button theme="link">link 按钮</Button>
-    <Button theme="text">text 按钮</Button>
+    <Button theme="link">链接按钮</Button>
+    <Button theme="text">文字按钮</Button>
   </div>
-  <h1>示例2</h1>
-  <div>
-    <div>
+  <h2>示例2 </h2>
+  <div class="box">
+    <div >
       <Button size="big">大大大</Button>
       <Button>普普通</Button>
       <Button size="small">小小小</Button>
@@ -25,6 +26,24 @@
       <Button size="small" theme="text">小小小</Button>
     </div>
   </div>
+  <h1>示例3</h1>
+  <div class="box">
+    <dir>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </dir>
+  </div>
 </template>
 
 <script lang='ts'>
@@ -35,4 +54,7 @@ export default {
 };
 </script>
 <style lang="scss" >
+  .box {
+    margin: 20px 0;
+  }
 </style>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,13 +16,19 @@ export default {
       type: String,
       default: "normal",
     },
+    level:{
+      type:String,
+      default:'normal'
+    }
   },
   setup(props) {
-    const { theme, size } = props;
+    const { theme, size ,level} = props;
     const classes = computed(() => {
       return {
         [`gulu-theme-${theme}`]: theme,
         [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
+
       };
     });
     return { classes };
@@ -35,6 +41,7 @@ export default {
   $color: #333;
   $blue: #40a9ff;
   $radius: 4px;
+  $red:red;
   .gulu-button {
     box-sizing: border-box;
     height: $h;
@@ -49,6 +56,7 @@ export default {
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
     & + & {
       margin-left: 8px;
     }
@@ -91,5 +99,52 @@ export default {
       height: 20px;
       padding: 0 4px;
     }
+     &.gulu-theme-button {
+    &.gulu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.gulu-theme-link {
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.gulu-theme-text {
+    &.gulu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
   }
 </style>
