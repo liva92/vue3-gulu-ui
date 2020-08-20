ebc0214630cd1277b7e6a07a5f71e79357bb06a3
liva <liva1992@163.com>
2020-08-20 16:01:54 +0800

    支持 disabled 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,15 +1,15 @@
 <template>
   <h1>Button 示例</h1>
 
-  <h2>示例1 </h2>
   <div class="box">
+  <h2>示例1 </h2>
     <Button>默认按钮</Button>
     <Button theme="button">button 按钮</Button>
     <Button theme="link">链接按钮</Button>
     <Button theme="text">文字按钮</Button>
   </div>
-  <h2>示例2 </h2>
   <div class="box">
+  <h2>示例2 </h2>
     <div >
       <Button size="big">大大大</Button>
       <Button>普普通</Button>
@@ -26,8 +26,9 @@
       <Button size="small" theme="text">小小小</Button>
     </div>
   </div>
-  <h1>示例3</h1>
+ 
   <div class="box">
+   <h2>示例3</h2>
     <dir>
       <Button level="main">主要按钮</Button>
       <Button>普通按钮</Button>
@@ -43,6 +44,13 @@
       <Button theme="text">普通文字按钮</Button>
       <Button theme="text" level="danger">危险文字按钮</Button>
     </dir>
+  </div>
+   
+  <div class="box">
+  <h2>示例4</h2>
+    <Button disabled>禁用按钮</Button>
+    <Button theme="link" disabled>禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用按钮</Button>
   </div>
 </template>
 
