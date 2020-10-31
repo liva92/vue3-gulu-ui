d93108655a0c2ed1e665b9ae2051ecfc52916eb1
liva <liva1992@163.com>
2020-10-31 09:48:35 +0800

    遇到 vue3 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,28 +1,86 @@
 <template>
-<div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index" />
+<div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+        <div class="gulu-tabs-nav-item" @click="select(t)" :class="{selected:t ===selected}" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="gulu-tabs-content">
+        {{current}}
+        <component class="gulu-tabs-content-item" :is="current" />
+    </div>
 </div>
 </template>
 
 <script lang="ts">
+import {
+    computed
+} from 'vue'
 import Tab from './Tab.vue'
 export default {
+    props: {
+        selected: {
+            type: String
+        }
+    },
     setup(props, context) {
+        //获取子组件对象集合
         const defaults = context.slots.default()
         defaults.forEach((tab) => {
             if (tab.type !== Tab) {
                 throw new Error('Tabs子标签必须是 Tab')
             }
         })
+        const current = computed(() => {
+            console.log('重新加载')
+            return defaults.filter((tag) => {
+                return tag.props.title === props.selected
+            })[0]
+        })
+        //获取子组件 title 数组
         const titles = defaults.map((tag) => {
             return tag.props.title
         })
+        const select = (title: String) => {
+            context.emit('update:selected', title)
+        }
         return {
             defaults,
-            titles
+            titles,
+            current,
+            select
         }
     }
 
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.gulu-tabs {
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $blue;
+            }
+        }
+    }
+
+    &-content {
+        padding: 8px 0;
+    }
+}
+</style>
